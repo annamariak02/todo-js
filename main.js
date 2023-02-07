172cbeca82736d897e16dfefab521ab4bbeb5a68
annamariak02 <annamaria.kenppi@gmail.com>
2023-02-07 18:18:38 +0200

    .

---
+++ main.js
@@ -0,0 +1,12 @@
+const myForm= document.querySelector('#my-form');
+const tekst= document.querySelector('#txtbox');
+const tasks= document.querySelector('#tasks');
+
+myForm.addEventListener('submit', onSubmit);
+function onSubmit(e){
+    e.preventDefault();
+    const li=document.createElement('li');
+    li.appendChild(document.createTextNode(`${tekst.value}`));
+    tasks.appendChild(li);
+    tekst.value='';
+}
