7da2999c68e380349a19cb79ce2b92549fa0c83b
annamariak02 <annamaria.kenppi@gmail.com>
2023-02-08 10:20:18 +0200

    added if statement

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ const tasks= document.querySelector('#tasks');
 myForm.addEventListener('submit', onSubmit);
 function onSubmit(e){
     e.preventDefault();
+    if(tekst.value !== ''){
     const li=document.createElement('li');
     li.appendChild(document.createTextNode(`${tekst.value}`));
     tasks.appendChild(li);
     tekst.value='';
+}
 }
